c918cf94fb9a0dc2fd2938b96eb83683393b81e1
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2024-04-24 00:51:42 +0700

    Update App.py

--- App.py
+++ App.py
@@ -3,8 +3,6 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.set_page_config(layout="wide")
 def wikipediaScrap(article_name, wikipedia_language="en"):
     wikipedia.set_lang(wikipedia_language)
     et_page = wikipedia.page(article_name)
@@ -12,31 +10,38 @@ def wikipediaScrap(article_name, wikipedia_language="en"):
     content = et_page.content
     page_url = et_page.url
     linked_pages = et_page.links
-    text = content
-
-    # Create and generate a word cloud image:
-    wordcloud = WordCloud(font_path="HelveticaWorld-Regular.ttf").generate(text)
-
-    # Display the generated image:
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
-    st.pyplot()
 
     return title, content, page_url, "\n".join(linked_pages)
 
-st.title("Wikipedia Article Scrape | Data Science Dojo")
-
-article_name = st.text_input("Enter the name of wikipedia article")
-wikipedia_language = st.selectbox("Select Language", ["en - English", "vi - Tiếng Việt"])
+st.title("Wikipedia Article Crawler")
+c1 = st.container(border=True)
+with c1:
+    article_name, wi_ln = st.columns(2)
+    with article_name:
+        article_name_input = st.text_input("Enter the name of wikipedia article")
+    with wi_ln:
+         ln = st.selectbox("Select Language", ["en - English", "vi - Tiếng Việt"])
 
 if st.button("Start scraping"):
-    title, content, url, linked_pages = wikipediaScrap(article_name, wikipedia_language.split(" - ")[0])
+    title, content, url, linked_pages = wikipediaScrap(article_name_input, ln.split(" - ")[0])
     st.subheader("About")
-    st.text("Article title:")
-    st.write(title)
-    st.text("Article URL:")
-    st.write(url)
+    c2 = st.container(border=True)
+    with c2:
+        title_col, plot_col = st.columns(2)
+        with title_col:
+            st.text("Article title:")
+            st.write(title)
+            st.text("Article URL:")
+            st.write(url)
+        with plot_col:
+            # Create and generate a word cloud image:
+            text = content
+            wordcloud = WordCloud(font_path="HelveticaWorld-Regular.ttf").generate(text)
+            # Display the generated image:
+            plt.figure(figsize=(10, 5))
+            plt.imshow(wordcloud, interpolation='bilinear')
+            plt.axis("off")
+            st.pyplot() 
     st.subheader("Content")
     st.write(content)
     st.subheader("Linked Articles")
