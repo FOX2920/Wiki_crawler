3d0b8658d9fa574d62f38ad943cdc2927b7e70de
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2024-04-24 00:42:32 +0700

    Update App.py

--- App.py
+++ App.py
@@ -18,8 +18,8 @@ def wikipediaScrap(article_name, wikipedia_language="en"):
 
 st.title("Wikipedia Article Crawler")
 c1 = st.container(border=True)
-    with c1:
-        article_name, wi-ln = st.columns(2)
+with c1:
+    article_name, wi-ln = st.columns(2)
     with article_name:
         article = st.text_input("Enter the name of wikipedia article")
     with wi-ln:
