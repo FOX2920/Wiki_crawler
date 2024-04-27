2e2b99b9db2c7d070993cb7be268520d7ff5b813
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2024-04-27 07:57:58 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -39,51 +39,54 @@ def format_content(content):
     formatted_content = '\n\n'.join(paragraphs)
     return formatted_content
 
+def main():
+    # Tiêu đề của ứng dụng
+    st.title("Ứng dụng Wikipedia Tiếng Việt")
+    
+    # Hộp nhập tiêu đề từ người dùng
+    title_input = st.text_input("Nhập tiêu đề bạn muốn tìm kiếm trên Wikipedia")
+    
+    # Trong phần hiển thị
+    if st.button("Start scraping"):
+        title, content, url, linked_df = wikipediaScrap(title_input)
+        st.subheader("About")
+        c2 = st.container(border=True)
+        with c2:
+            plot_col, title_col = st.columns(2)
+            with title_col:
+                st.subheader("Article title:")
+                st.write(title)
+                st.subheader("Article URL:")
+                st.write(url)
+                st.subheader("Linked Articles:")
+                st.dataframe(linked_df, height=200, use_container_width=True)  # Hiển thị DataFrame thay vì hiển thị list
+            with plot_col:
+                # Create and generate a word cloud image:
+                text = content
+                st.subheader("WordCloud")
+                wordcloud = WordCloud(font_path="HelveticaWorld-Regular.ttf").generate(text)
+                # Display the generated image:
+                plt.figure(figsize=(10, 5))
+                plt.imshow(wordcloud, interpolation='bilinear')
+                plt.axis("off")
+                st.pyplot() 
+        st.subheader("Content")
+        c3 = st.container(border=True)
+        with c3:
+            content_col, df_col = st.columns(2)
+            with  content_col:
+                st.subheader("Summary:")
+                # Xử lý nội dung trước khi hiển thị
+                formatted_content = format_content(content)
+                st.text_area('',value=formatted_content, height=500, max_chars= 500)
+            with df_col:
+                # Chia tóm tắt thành các câu
+                sentences = nltk.sent_tokenize(content)
+                
+                # Tạo DataFrame từ các câu
+                df = pd.DataFrame(sentences, columns=["Câu"])
+                st.subheader("Các câu trong tóm tắt:")
+                st.dataframe(df, height=500, use_container_width=True, hide_index=True)
 
-# Tiêu đề của ứng dụng
-st.title("Ứng dụng Wikipedia Tiếng Việt")
-
-# Hộp nhập tiêu đề từ người dùng
-title_input = st.text_input("Nhập tiêu đề bạn muốn tìm kiếm trên Wikipedia")
-
-# Trong phần hiển thị
-if st.button("Start scraping"):
-    title, content, url, linked_df = wikipediaScrap(title_input)
-    st.subheader("About")
-    c2 = st.container(border=True)
-    with c2:
-        plot_col, title_col = st.columns(2)
-        with title_col:
-            st.subheader("Article title:")
-            st.write(title)
-            st.subheader("Article URL:")
-            st.write(url)
-            st.subheader("Linked Articles:")
-            st.dataframe(linked_df, height=200, use_container_width=True)  # Hiển thị DataFrame thay vì hiển thị list
-        with plot_col:
-            # Create and generate a word cloud image:
-            text = content
-            st.subheader("WordCloud")
-            wordcloud = WordCloud(font_path="HelveticaWorld-Regular.ttf").generate(text)
-            # Display the generated image:
-            plt.figure(figsize=(10, 5))
-            plt.imshow(wordcloud, interpolation='bilinear')
-            plt.axis("off")
-            st.pyplot() 
-    st.subheader("Content")
-    c3 = st.container(border=True)
-    with c3:
-        content_col, df_col = st.columns(2)
-        with  content_col:
-            st.subheader("Summary:")
-            # Xử lý nội dung trước khi hiển thị
-            formatted_content = format_content(content)
-            st.text_area('',value=formatted_content, height=500, max_chars= 500)
-        with df_col:
-            # Chia tóm tắt thành các câu
-            sentences = nltk.sent_tokenize(content)
-            
-            # Tạo DataFrame từ các câu
-            df = pd.DataFrame(sentences, columns=["Câu"])
-            st.subheader("Các câu trong tóm tắt:")
-            st.dataframe(df, height=500, use_container_width=True, hide_index=True)
+if __name__ == "__main__":
+    main()
